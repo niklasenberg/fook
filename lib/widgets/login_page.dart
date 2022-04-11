5a616f46296ff5a9438a7fed2f0148d26ba8bda7
Niklas Enberg <nickeen95@gmail.com>
2022-04-11 15:05:49 +0200

    update login_page.dart

--- lib/widgets/login_page.dart
+++ lib/widgets/login_page.dart
@@ -1,7 +1,8 @@
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:flutter/material.dart';
 import 'home_page.dart';
 import 'package:flutterfire_ui/auth.dart';
+import 'package:string_validator/string_validator.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -9,6 +10,7 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       body: LoginForm(),
     );
   }
@@ -19,6 +21,7 @@ class LoginForm extends StatelessWidget {
 
   final emailCtrl = TextEditingController();
   final passwordCtrl = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -27,79 +30,100 @@ class LoginForm extends StatelessWidget {
         if (state is SignedIn) {
           Navigator.pushReplacement(context,
               MaterialPageRoute(builder: (context) => const HomePage()));
+        } else if (state is AuthFailed) {
+          Fluttertoast.showToast(
+              msg: 'Login or password is invalid',
+              toastLength: Toast.LENGTH_SHORT,
+              gravity: ToastGravity.CENTER,
+              timeInSecForIosWeb: 2,
+              backgroundColor: Colors.deepOrangeAccent,
+              textColor: Colors.white,
+              fontSize: 16.0);
         }
       },
       builder: (context, state, controller, _) {
         if (state is AwaitingEmailAndPassword) {
-          return Column(
-            children: [
-              Container(
-                width: 300.0,
-                height: 300.0,
-                child: FittedBox(
-                  fit: BoxFit.contain,
-                  child: Image.asset(
-                    'lib/assets/logo_o.png',
-                    scale: 0.5,
+          return Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                Container(
+                  width: 300.0,
+                  height: 300.0,
+                  child: FittedBox(
+                    fit: BoxFit.contain,
+                    child: Image.asset(
+                      'lib/assets/logo_o.png',
+                      scale: 0.5,
+                    ),
                   ),
                 ),
-              ),
-              Container(
-                padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
-                child: const Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    'Sign in with Daisy',
-                    textAlign: TextAlign.left,
-                    style:
-                        TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
+                Container(
+                  padding:
+                      EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
+                  child: const Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      'Sign in with Daisy',
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, fontSize: 20.0),
+                    ),
                   ),
                 ),
-              ),
-              Column(
-                children: [
-                  Container(
-                    padding: EdgeInsets.symmetric(horizontal: 10.0),
-                    child: TextFormField(
-                        decoration: const InputDecoration(
-                          border: OutlineInputBorder(),
-                          labelText: 'Username',
-                        ),
-                        controller: emailCtrl),
-                  ),
-                  const Padding(padding: EdgeInsets.symmetric(vertical: 16.0)),
-                  Container(
-                    padding: EdgeInsets.symmetric(horizontal: 10.0),
-                    child: TextFormField(
-                        obscureText: true,
-                        decoration: const InputDecoration(
-                          border: OutlineInputBorder(),
-                          labelText: 'Password',
-                        ),
-                        controller: passwordCtrl),
-                  ),
-                ],
-              ),
-              Padding(padding: EdgeInsets.symmetric(vertical: 16.0)),
-              ElevatedButton(
-                onPressed: () {
-                  controller.setEmailAndPassword(
-                    emailCtrl.text,
-                    passwordCtrl.text,
-                  );
-                },
-                child: const Text('Sign in'),
-                style: ElevatedButton.styleFrom(
-                  fixedSize: Size(120.0, 30.0),
+                Column(
+                  children: [
+                    Container(
+                      padding: EdgeInsets.symmetric(horizontal: 10.0),
+                      child: TextFormField(
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter some text';
+                            } else if (value.length != 8 || !isAlpha(value.substring(0,3)) || !isNumeric(value.substring(4,7))) {
+                              return 'Wrong format, should be abcd1234';
+                            }
+                          },
+                          decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Username',
+                          ),
+                          controller: emailCtrl),
+                    ),
+                    const Padding(
+                        padding: EdgeInsets.symmetric(vertical: 16.0)),
+                    Container(
+                      padding: EdgeInsets.symmetric(horizontal: 10.0),
+                      child: TextFormField(
+                          obscureText: true,
+                          decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Password',
+                          ),
+                          controller: passwordCtrl),
+                    ),
+                  ],
                 ),
-              ),
-            ],
+                Padding(padding: EdgeInsets.symmetric(vertical: 16.0)),
+                ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      controller.setEmailAndPassword(
+                        emailCtrl.text + '@test.com',
+                        passwordCtrl.text,
+                      );
+                    }
+                  },
+                  child: const Text('Sign in'),
+                  style: ElevatedButton.styleFrom(
+                    fixedSize: Size(120.0, 30.0),
+                    primary: Colors.deepOrangeAccent,
+                  ),
+                ),
+              ],
+            ),
           );
         } else if (state is SigningIn) {
           return Center(child: CircularProgressIndicator());
-        } else if (state is AuthFailed) {
-          // FlutterFireUIWidget that shows a human-readable error message.
-          return SnackBar(content: Text(state.exception.toString()));
         } else {
           return const LoginPage();
         }
