73fa426d0ca95975912772b24fa93af2c5054437
joel bodin <dorraavila@hotmail.com>
2022-05-10 19:39:24 +0200

    Myrsteg
    Myrsteg

---
+++ lib/widgets/sale/sale_create_new.dart
@@ -0,0 +1,11 @@
+/*//import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+//import 'package:cloud_firestore/cloud_firestore.dart';
+
+class SaleCreateNew extends StatefulWidget {
+  const SaleCreateNew({Key? key}) : super(key: key);
+
+  @override
+  State<SaleCreateNew> createState() => _SaleHomePageState();
+}*/
