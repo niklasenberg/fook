465564ea0192447b364c719320ff060c656765a6
Mandem19 <jeremydemirel98@gmail.com>
2022-05-10 16:19:30 +0200

    Progress salepage

--- lib/widgets/sale_page.dart
+++ lib/widgets/sale_page.dart
@@ -1,5 +1,6 @@
 //import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 //import 'package:cloud_firestore/cloud_firestore.dart';
 
 class SalePage extends StatefulWidget {
@@ -24,15 +25,47 @@ class _SalePageState extends State<SalePage> {
           child: Column(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
+            SizedBox(
+              height: 300,
+              width: 300,
+              child: LimitedBox(
+                maxHeight: 160,
+                maxWidth: 160,
+                child: Container(
+                    color: Colors.grey,
+                    child: Container(
+                      child: myBooksales(),
+                    )),
+              ),
+              //lägga in en lista av alla ens böcker till försälning
+              //om listan är tom ska det stå 'no ads to be shown'
+            ),
             Container(
-                decoration: const BoxDecoration(
-                  color: Colors.blue,
-                ),
-                child: const Text('No ads to be shown')),
-            Container(
-                decoration: const BoxDecoration(
-                  color: Colors.blue,
-                ),
-                child: const Text('Create new'))
+                alignment: Alignment.center,
+                padding: const EdgeInsets.all(32),
+                child: ElevatedButton.icon(
+                  icon: const Text('Create new'),
+                  label: const Icon(Icons.add_business),
+                  onPressed: () => Fluttertoast.showToast(
+                    //handler för skapa anons ska in här
+                    msg: 'lägg in fkn handler bro',
+                    fontSize: 10,
+                  ),
+                ))
           ])));
+
+  Widget myBooksales() => ListView(
+        children: const [
+          ListTile(
+            leading: Icon(Icons.book),
+            title: Text('Bok 1'),
+            subtitle: Text('info1'),
+          ),
+          ListTile(
+            leading: Icon(Icons.book),
+            title: Text('Bok 2'),
+            subtitle: Text('info2'),
+          ),
+        ],
+      );
 }
