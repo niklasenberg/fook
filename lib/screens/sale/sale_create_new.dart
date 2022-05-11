4f8c9d499ee12f9ec4ca70562492091b08f12038
joel bodin <dorraavila@hotmail.com>
2022-05-11 20:52:09 +0200

    Modifications

--- lib/screens/sale/sale_create_new.dart
+++ lib/screens/sale/sale_create_new.dart
@@ -1,6 +1,7 @@
 //import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:fook/screens/sale/widgets/rounded_app_bar.dart';
 //import 'package:cloud_firestore/cloud_firestore.dart';
 
 class SaleCreateNew extends StatefulWidget {
@@ -13,12 +14,19 @@ class SaleCreateNew extends StatefulWidget {
 class _SaleCreateNewState extends State<SaleCreateNew> {
   @override
   Widget build(BuildContext context) => Scaffold(
-      appBar: AppBar(
+      appBar:AppBar(
           shape: const RoundedRectangleBorder(
               borderRadius: BorderRadius.only(
                   bottomLeft: Radius.circular(20),
                   bottomRight: Radius.circular(20))),
-          title: const Text('MY SALES', style: TextStyle(color: Colors.orange)),
+          title:  Text("SKAPA ANNONS", style: TextStyle(color: Colors.orange)),
           centerTitle: true,
-          backgroundColor: Colors.white
-          ));}
+          bottom: PreferredSize(
+                  child: Container(padding: const EdgeInsets.fromLTRB(18, 0, 0, 5),
+                    alignment: Alignment.centerLeft,
+                    child: const Text("Titel och författare hämtas automatiskt via ISBN-numret",
+                    style: TextStyle(color: Colors.black),),
+                  ),
+                  preferredSize: const Size.fromHeight(1),
+                ),
+          backgroundColor: Colors.white),);}
